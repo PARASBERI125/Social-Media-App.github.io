8b43d33523d4112088a137fe7ec1fa965dffef8b
paras beri <130133581+PARASBERI125@users.noreply.github.com>
2024-08-13 20:46:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Specify the port you want to use
+    strictPort: true, // Optional: ensures Vite fails if the port is in use
+  },
+  preview: {
+    port: 3000, // Specify the port for preview mode
+    strictPort: true, // Optional: ensures preview mode fails if the port is in use
+  },
+});
